9a86cb24545d58cfd5f2a852d12ef71b15015d0d
Sergey Golovkin <kefiriaus@gmail.com>
2017-01-09 13:52:55 +0800

    Update decorators

--- app/decorators/about_decorator.rb
+++ app/decorators/about_decorator.rb
@@ -1,4 +1,4 @@
-class AboutDecorator < Drape::Decorator
+class AboutDecorator < Draper::Decorator
   delegate_all
 
   # Define presentation-specific methods here. Helpers are accessed through
--- app/decorators/contact_decorator.rb
+++ app/decorators/contact_decorator.rb
@@ -1,4 +1,4 @@
-class ContactDecorator < Drape::Decorator
+class ContactDecorator < Draper::Decorator
   delegate_all
 
   # Define presentation-specific methods here. Helpers are accessed through
